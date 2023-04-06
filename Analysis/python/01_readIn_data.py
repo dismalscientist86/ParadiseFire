e69d6010d3bfaf70b1924bcb63c630b45ee2ae0f
dismalscientist86 <danielle.h.sandler@census.gov>
2023-04-06 07:21:36 -0400

    Update 01_readIn_data.py

--- Analysis/python/01_readIn_data.py
+++ Analysis/python/01_readIn_data.py
@@ -7,39 +7,94 @@ import pandas as pd
 if os.getlogin() == "sandl305":
     programs = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\programs"
     data = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data"
-    output = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\"
+    output = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis"
     logs = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\programs\logs"
     graphs = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\graphs"
-	
-url = "https://lehd.ces.census.gov/data/lodes/LODES8/ca/od/ca_od_main_JT00_2020.csv.gz"
-file_path = os.path.join(data, "ca_od_main_JT00_2020.csv.gz")
+    data_od = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_od"
+    data_rac = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_rac"
+    data_wac = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_wac" 
+    
+#Bring in Origin-Destination Main dataset, for All Jobs (JT00)
+#Note - I do not understand why having the range go through 2021 means files are downloaded through 2020, but if the range went to 2020 it only downloaded up to 2019
+for i in range(2013,2021):	
+    url = f"https://lehd.ces.census.gov/data/lodes/LODES8/ca/od/ca_od_main_JT00_{i}.csv.gz"
+    file_path = os.path.join(data_od, f"ca_od_main_JT00_{i}.csv.gz")
 
-response = requests.get(url, stream=True)
-with open(file_path, "wb") as f:
-    shutil.copyfileobj(response.raw, f)
+    response = requests.get(url, stream=True)
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(response.raw, f)
 
-with gzip.open(file_path, "rb") as f_in:
-    with open(os.path.splitext(file_path)[0], "wb") as f_out:
-        shutil.copyfileobj(f_in, f_out)
+    with gzip.open(file_path, "rb") as f_in:
+        with open(os.path.splitext(file_path)[0], "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
 
-#Bring in Origin-Destination Main dataset, for All Jobs (JT00)		
-# Read in CSV file
-df = pd.read_csv(file_path)
+    # Read in CSV file
+    df = pd.read_csv(file_path, dtype=str)
 
-# Convert geocodes to string variables
-df["w_geocode"] = df["w_geocode"].astype(str).str.pad(width=15, fillchar="0")
-df["h_geocode"] = df["h_geocode"].astype(str).str.pad(width=15, fillchar="0")
+    # Create county FIPS from first five digits of geocode
+    df["w_county"] = df["w_geocode"].str[:5]
+    df["h_county"] = df["h_geocode"].str[:5]
 
-# Add a leading zero to geocodes
-df["w_geocode"] = "0" + df["w_geocode"]
-df["h_geocode"] = "0" + df["h_geocode"]
+    # Keep only Butte County (FIPS=06007)
+    df = df[(df["w_county"] == "06007") | (df["h_county"] == "06007")]
+    
+    # Save resulting data as CSV file
+    df.to_csv(os.path.join(data_od, f"od_main_JT00_{i}.csv"), index=False)
+    
+#Bring in Residence Area Characteristics Main dataset, for All Jobs (JT00)
+for i in range(2013,2021):	
+    url = f"https://lehd.ces.census.gov/data/lodes/LODES8/ca/rac/ca_rac_S000_JT00_{i}.csv.gz"
+    file_path = os.path.join(data_rac, f"ca_rac_S000_JT00_{i}.csv.gz")
 
-# Create county FIPS from first five digits of geocode
-df["w_county"] = df["w_geocode"].str[:5]
-df["h_county"] = df["h_geocode"].str[:5]
+    response = requests.get(url, stream=True)
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(response.raw, f)
 
-# Keep only Butte County (FIPS=06007)
-df = df.loc[(df["w_county"] == "06007") | (df["h_county"] == "06007")]
+    with gzip.open(file_path, "rb") as f_in:
+        with open(os.path.splitext(file_path)[0], "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
 
-# Save resulting data as CSV file
-df.to_csv(os.path.join(data_dir, "od_main_JT00_2020.csv"), index=False)
+    # Read in CSV file
+    df = pd.read_csv(file_path, dtype=str)
+
+    # Create county FIPS from first five digits of geocode
+    df["h_county"] = df["h_geocode"].str[:5]
+
+    # Keep only Butte County (FIPS=06007)
+    df = df[(df["h_county"] == "06007")]
+    
+    # Save resulting data as CSV file
+    df.to_csv(os.path.join(data_rac, f"rac_S000_JT00_{i}.csv"), index=False)
+    
+#Bring in Work Area Characteristics Main dataset, for All Jobs (JT00)
+for i in range(2013,2021):	
+    url = f"https://lehd.ces.census.gov/data/lodes/LODES8/ca/wac/ca_wac_S000_JT00_{i}.csv.gz"
+    file_path = os.path.join(data_wac, f"ca_wac_S000_JT00_{i}.csv.gz")
+
+    response = requests.get(url, stream=True)
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(response.raw, f)
+
+    with gzip.open(file_path, "rb") as f_in:
+        with open(os.path.splitext(file_path)[0], "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+    # Read in CSV file
+    df = pd.read_csv(file_path, dtype=str)
+
+    # Create county FIPS from first five digits of geocode
+    df["w_county"] = df["w_geocode"].str[:5]
+
+    # Keep only Butte County (FIPS=06007)
+    df = df[(df["w_county"] == "06007")]
+    
+    # Save resulting data as CSV file
+    df.to_csv(os.path.join(data_wac, f"wac_S000_JT00_{i}.csv"), index=False)
+    
+#Append data from years 2013-2020 and flag Paradise geocodes
+wac_files=[os.path.join(data_wac,f"wac_S000_JT00_{i}.csv") for i in range(2013,2020)]
+
+#Read in first year and create year variable
+wac_df=pd.read_csv(wac_files[0])
+wac_df["year"]=2013
+
