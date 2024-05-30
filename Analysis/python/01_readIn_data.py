a5ba01a38c641774b6108ffde781d45d0e186deb
dismalscientist86 <danielle.h.sandler@census.gov>
2024-05-29 20:14:08 -0700

    Minor code improvements

--- Analysis/python/01_readIn_data.py
+++ Analysis/python/01_readIn_data.py
@@ -5,18 +5,17 @@ import shutil
 import pandas as pd
 
 if os.getlogin() == "sandl305":
-    programs = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\programs"
-    data = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data"
-    output = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis"
-    logs = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\programs\logs"
-    graphs = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\graphs"
-    data_od = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_od"
-    data_rac = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_rac"
-    data_wac = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_wac" 
-    
-#Bring in Origin-Destination Main dataset, for All Jobs (JT00)
-#Note - I do not understand why having the range go through 2021 means files are downloaded through 2020, but if the range went to 2020 it only downloaded up to 2019
-for i in range(2013,2021):	
+    programs = r"C:\\Users\\sandl305\\Documents\\GitHub\\ParadiseFire\\Analysis\\programs"
+    data = r"C:\\Users\\sandl305\\Documents\\GitHub\\ParadiseFire\\Analysis\\data"
+    output = r"C:\\Users\\sandl305\\Documents\\GitHub\\ParadiseFire\\Analysis"
+    logs = r"C:\\Users\\sandl305\\Documents\\GitHub\\ParadiseFire\\Analysis\\programs\\logs"
+    graphs = r"C:\\Users\\sandl305\\Documents\\GitHub\\ParadiseFire\\Analysis\\graphs"
+    data_od = r"C:\\Users\\sandl305\\Documents\\GitHub\\ParadiseFire\\Analysis\\data\\lodes_od"
+    data_rac = r"C:\\Users\\sandl305\\Documents\\GitHub\\ParadiseFire\\Analysis\\data\\lodes_rac"
+    data_wac = r"C:\\Users\\sandl305\\Documents\\GitHub\\ParadiseFire\\Analysis\\data\\lodes_wac"
+
+# Download and process Origin-Destination Main dataset (2013-2020)
+for i in range(2013, 2021):
     url = f"https://lehd.ces.census.gov/data/lodes/LODES8/ca/od/ca_od_main_JT00_{i}.csv.gz"
     file_path = os.path.join(data_od, f"ca_od_main_JT00_{i}.csv.gz")
 
@@ -28,118 +27,57 @@ for i in range(2013,2021):
         with open(os.path.splitext(file_path)[0], "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-    # Read in CSV file
-    df = pd.read_csv(file_path, dtype=str)
-
-    # Create county FIPS from first five digits of geocode
+    # Read and filter CSV file
+    df = pd.read_csv(os.path.splitext(file_path)[0], dtype=str)
     df["w_county"] = df["w_geocode"].str[:5]
     df["w_tract"] = df["w_geocode"].str[:9]
     df["h_county"] = df["h_geocode"].str[:5]
     df["h_tract"] = df["h_geocode"].str[:9]
-
-    # Keep only Butte County (FIPS=06007)
     df = df[(df["w_county"] == "06007") | (df["h_county"] == "06007")]
-    
-    # Save resulting data as CSV file
+
+    # Save processed file
     df.to_csv(os.path.join(data_od, f"od_main_JT00_{i}.csv"), index=False)
-    
-#Bring in Residence Area Characteristics Main dataset, for All Jobs (JT00)
-for i in range(2013,2021):	
-    url = f"https://lehd.ces.census.gov/data/lodes/LODES8/ca/rac/ca_rac_S000_JT00_{i}.csv.gz"
-    file_path = os.path.join(data_rac, f"ca_rac_S000_JT00_{i}.csv.gz")
 
-    response = requests.get(url, stream=True)
-    with open(file_path, "wb") as f:
-        shutil.copyfileobj(response.raw, f)
-
-    with gzip.open(file_path, "rb") as f_in:
-        with open(os.path.splitext(file_path)[0], "wb") as f_out:
-            shutil.copyfileobj(f_in, f_out)
-
-    # Read in CSV file
-    df = pd.read_csv(file_path, dtype=str)
-
-    # Create county FIPS from first five digits of geocode
-    df["h_county"] = df["h_geocode"].str[:5]
-    df["h_tract"] = df["h_geocode"].str[:9]
-
-    # Keep only Butte County (FIPS=06007)
-    df = df[(df["h_county"] == "06007")]
-    
-    # Save resulting data as CSV file
-    df.to_csv(os.path.join(data_rac, f"rac_S000_JT00_{i}.csv"), index=False)
-    
-#Bring in Work Area Characteristics Main dataset, for All Jobs (JT00)
-for i in range(2013,2021):	
-    url = f"https://lehd.ces.census.gov/data/lodes/LODES8/ca/wac/ca_wac_S000_JT00_{i}.csv.gz"
-    file_path = os.path.join(data_wac, f"ca_wac_S000_JT00_{i}.csv.gz")
-
-    response = requests.get(url, stream=True)
-    with open(file_path, "wb") as f:
-        shutil.copyfileobj(response.raw, f)
-
-    with gzip.open(file_path, "rb") as f_in:
-        with open(os.path.splitext(file_path)[0], "wb") as f_out:
-            shutil.copyfileobj(f_in, f_out)
-
-    # Read in CSV file
-    df = pd.read_csv(file_path, dtype=str)
-
-    # Create county FIPS from first five digits of geocode
-    df["w_county"] = df["w_geocode"].str[:5]
-    df["w_tract"] = df["w_geocode"].str[:9]
-
-    # Keep only Butte County (FIPS=06007)
-    df = df[(df["w_county"] == "06007")]
-    
-    # Save resulting data as CSV file
-    df.to_csv(os.path.join(data_wac, f"wac_S000_JT00_{i}.csv"), index=False)
-    
-#Append data from years 2013-2020 and flag Paradise geocodes
-#Origin-Destination
-od_df = pd.read_csv(os.path.join(data_od,'od_main_JT00_2013.csv'), dtype=str)
+# Append data and flag Paradise geocodes
+od_df = pd.read_csv(os.path.join(data_od, 'od_main_JT00_2013.csv'), dtype=str)
 od_df['year'] = 2013
 
 for year in range(2014, 2021):
-    od_year = pd.read_csv(os.path.join(data_od, f"od_main_JT00_{year}.csv"), dtype=str)
+    od_year = pd.read_csv(os.path.join(data_od, f'od_main_JT00_{year}.csv'), dtype=str)
     od_year['year'] = year
-    od_df = pd.concat([od_df,od_year], ignore_index=True)
-    od_df.loc[od_df['year'].isnull(), 'year'] = year
+    od_df = pd.concat([od_df, od_year], ignore_index=True)
 
 od_df['paradise'] = 0
-od_df.loc[od_df['h_tract'].isin(["060070018", "060070019", "060070020", "060070021"]), 'paradise'] = 1
-od_df.loc[od_df['w_tract'].isin(["060070018", "060070019", "060070020", "060070021"]), 'paradise'] = 1
+paradise_tracts = ["060070018", "060070019", "060070020", "060070021"]
+od_df.loc[od_df['h_tract'].isin(paradise_tracts), 'paradise'] = 1
+od_df.loc[od_df['w_tract'].isin(paradise_tracts), 'paradise'] = 1
 
+od_df.to_csv(os.path.join(data, "od_2013_2020.csv"), index=False)
 
-od_df.to_csv(os.path.join(data, "od_2013_2020.csv"))
-
-#Work Area Characteristics
-wac_df = pd.read_csv(os.path.join(data_wac,'wac_S000_JT00_2013.csv'), dtype=str)
+# Process Work Area Characteristics data
+wac_df = pd.read_csv(os.path.join(data_wac, 'wac_S000_JT00_2013.csv'), dtype=str)
 wac_df['year'] = 2013
 
 for year in range(2014, 2021):
-    wac_year = pd.read_csv(os.path.join(data_wac, f"wac_S000_JT00_{year}.csv"),dtype=str)
+    wac_year = pd.read_csv(os.path.join(data_wac, f'wac_S000_JT00_{year}.csv'), dtype=str)
     wac_year['year'] = year
-    wac_df = pd.concat([wac_df,wac_year], ignore_index=True)
+    wac_df = pd.concat([wac_df, wac_year], ignore_index=True)
 
 wac_df['paradise'] = 0
-wac_df.loc[wac_df['w_tract'].isin(["060070018", "060070019", "060070020", "060070021"]), 'paradise'] = 1
-freq_table = wac_df['paradise'].value_counts()
-print(freq_table)
+wac_df.loc[wac_df['w_tract'].isin(paradise_tracts), 'paradise'] = 1
 
-wac_df.to_csv(os.path.join(data, "wac_2013_2020.csv"))
+wac_df.to_csv(os.path.join(data, "wac_2013_2020.csv"), index=False)
 
-#Residence Area Characteristics
-rac_df = pd.read_csv(os.path.join(data_rac,'rac_S000_JT00_2013.csv'),dtype=str)
+# Process Residence Area Characteristics data
+rac_df = pd.read_csv(os.path.join(data_rac, 'rac_S000_JT00_2013.csv'), dtype=str)
 rac_df['year'] = 2013
 
 for year in range(2014, 2021):
-    rac_year = pd.read_csv(os.path.join(data_rac, f"rac_S000_JT00_{year}.csv"),dtype=str)
+    rac_year = pd.read_csv(os.path.join(data_rac, f'rac_S000_JT00_{year}.csv'), dtype=str)
     rac_year['year'] = year
-    rac_df = pd.concat([rac_df,rac_year], ignore_index=True)
-    rac_df.loc[rac_df['year'].isnull(), 'year'] = year
+    rac_df = pd.concat([rac_df, rac_year], ignore_index=True)
 
 rac_df['paradise'] = 0
-rac_df.loc[rac_df['h_tract'].isin(["060070018", "060070019", "060070020", "060070021"]), 'paradise'] = 1
+rac_df.loc[rac_df['h_tract'].isin(paradise_tracts), 'paradise'] = 1
 
-rac_df.to_csv(os.path.join(data, "rac_2013_2020.csv"))
+rac_df.to_csv(os.path.join(data, "rac_2013_2020.csv"), index=False)
--- Analysis/python/03_workarea_graphs.py
+++ Analysis/python/03_workarea_graphs.py
@@ -10,54 +10,50 @@ if os.getlogin() == "sandl305":
     graphs = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\graphs"
     data_od = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_od"
     data_rac = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_rac"
-    data_wac = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_wac" 
-   
+    data_wac = r"C:\Users\sandl305\Documents\GitHub\ParadiseFire\Analysis\data\lodes_wac"
 
-# read in data
+# Read in data
 wac = pd.read_csv(os.path.join(data, "wac_2013_2020.csv"))
 
-freq_table = wac['paradise'].value_counts()
-print(freq_table)
+# Print frequency table and crosstab
+print(wac['paradise'].value_counts())
+print(pd.crosstab(wac['year'], wac['paradise']))
 
-table=pd.crosstab(wac['year'],wac['paradise'])
-print(table)
-
-# group by paradise and year and sum all count variables
+# Group by paradise and year, sum all count variables
 wac_grouped = wac.groupby(['paradise', 'year'], as_index=False).sum()
 
-#create list of variables to loop over
-jobvars =['C000','CA01','CA02','CA03','CE01','CE02','CE03','CNS01','CNS02','CNS03',
-          'CNS04','CNS05','CNS06','CNS07','CNS08','CNS09','CNS10','CNS11','CNS12',
-          'CNS13','CNS14','CNS15','CNS16','CNS17','CNS18','CNS19','CNS20','CR01',
-          'CR02','CR03','CR04','CR05','CT01','CT02','CD01','CD02','CD03','CD04',
-          'CS01','CS02']
-#Keep only 2017, to use as base year for percentage change
-wac_grouped_2017=wac_grouped[wac_grouped['year']==2017]
-wac_grouped_2017=wac_grouped_2017.loc[:,['paradise']+ jobvars]
-#Create a dictorary to append _2017 to all of the variables names
-rename_dict={col:col + '_2017' for col in jobvars}
-wac_grouped_2017=wac_grouped_2017.rename(columns=rename_dict)
+# List of variables to loop over
+jobvars = [
+    'C000', 'CA01', 'CA02', 'CA03', 'CE01', 'CE02', 'CE03', 'CNS01', 'CNS02', 'CNS03',
+    'CNS04', 'CNS05', 'CNS06', 'CNS07', 'CNS08', 'CNS09', 'CNS10', 'CNS11', 'CNS12',
+    'CNS13', 'CNS14', 'CNS15', 'CNS16', 'CNS17', 'CNS18', 'CNS19', 'CNS20', 'CR01',
+    'CR02', 'CR03', 'CR04', 'CR05', 'CT01', 'CT02', 'CD01', 'CD02', 'CD03', 'CD04',
+    'CS01', 'CS02'
+]
 
-#Merge 2017 values to main dataframe
-merged_df=pd.merge(wac_grouped,wac_grouped_2017, on='paradise')
+# Keep only 2017, to use as base year for percentage change
+wac_grouped_2017 = wac_grouped[wac_grouped['year'] == 2017].loc[:, ['paradise'] + jobvars]
+rename_dict = {col: col + '_2017' for col in jobvars}
+wac_grouped_2017 = wac_grouped_2017.rename(columns=rename_dict)
 
-#Calculate percent change from 2017
+# Merge 2017 values to main dataframe
+merged_df = pd.merge(wac_grouped, wac_grouped_2017, on='paradise')
+
+# Calculate percent change from 2017 and create line charts
 for var in jobvars:
-    merged_df[var+'_pctchg']=(merged_df[var]-merged_df[var+'_2017'])/merged_df[var+'_2017']
+    merged_df[var + '_pctchg'] = (merged_df[var] - merged_df[var + '_2017']) / merged_df[var + '_2017']
 
-    #Create line chart
-    fig, ax=plt.subplots()
-    #Filter data for paradise=1 and plot line
-    paradise1=merged_df[merged_df['paradise']==1]
-    ax.plot(paradise1['year'],paradise1[var+'_pctchg'],marker='o',label='Paradise')
-    #Filter data for paradise=0 and plot line
-    paradise0=merged_df[merged_df['paradise']==0]
-    ax.plot(paradise0['year'],paradise0[var+'_pctchg'],marker='o',label='Rest of Butte County')
+    fig, ax = plt.subplots()
+    paradise1 = merged_df[merged_df['paradise'] == 1]
+    paradise0 = merged_df[merged_df['paradise'] == 0]
+
+    ax.plot(paradise1['year'], paradise1[var + '_pctchg'], marker='o', label='Paradise')
+    ax.plot(paradise0['year'], paradise0[var + '_pctchg'], marker='o', label='Rest of Butte County')
+
     ax.set_xlabel('Year')
     ax.set_ylabel('Percent Change')
-    ax.set_title('Percent Change in Jobs (Base 2017) '+var)
+    ax.set_title(f'Percent Change in Jobs (Base 2017) {var}')
     ax.legend()
-    fig=plt.gcf()
-    plt.show()
-    fig.savefig(os.path.join(graphs,var+'_workarea.png'))
 
+    plt.show()
+    fig.savefig(os.path.join(graphs, f'{var}_workarea.png'))
